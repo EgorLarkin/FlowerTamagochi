ed2a9d20422ca067a19c8d787ca7f63eae088fff
EgorLarkin <166232709+EgorLarkin@users.noreply.github.com>
2025-09-28 13:29:03 +0300

    Update ContentView.swift

--- Flower Tamagochi final/Flower Tamagochi/ContentView.swift
+++ Flower Tamagochi final/Flower Tamagochi/ContentView.swift
@@ -21,6 +21,8 @@ struct ContentView: View {
     @State private var temp: Int?
     @State private var airHumidity: Int?
     @State private var soilHumidity: Int?
+    @State private var showStats: Bool = false
+    @State private var countEdits: Int = 0
 //    @ObservedObject var bluetoothManager = BluetoothManager()
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
@@ -87,9 +89,7 @@ struct ContentView: View {
                 .frame(width: 500)
                 .offset(y: -100)
                 Button(
-                    action: {
-                        
-                    }, label: {
+                    action: {showStats = true}, label: {
                         Text(" Статистика цветочка ")
                             .background(Color.blue)
                             .foregroundStyle(.white)
@@ -105,6 +105,7 @@ struct ContentView: View {
             .onChange(of: flowerName) { newValue in
                 writeNewName()
             }
+            .sheet(isPresented: $showStats){StatsModal()}
             .onChange(of: temp) { newValue in
                 if temp ?? 0 >= 15 && temp ?? 0 <= 25 && airHumidity ?? 0 >= 40 && airHumidity ?? 0 <= 70 && soilHumidity ?? 0 >= 20 && soilHumidity ?? 0 <= 50 {
                     chatRecommendation = "Все хорошо!"
@@ -122,8 +123,18 @@ struct ContentView: View {
                     )
                 }
             }
+            .onChange(of: countEdits) { newValue in
+                writeNewCount()
+                if countEdits >= 10 {
+                    shrinkFile()
+                    countEdits = 0
+                }
+                
+            }
         }
         .onAppear() {
+            countEdits = Int(readEdits())!
+            print(countEdits)
             flowerName = readFromFile()
             writeNewData(
                 temp: temp ?? 0,
@@ -131,6 +142,7 @@ struct ContentView: View {
                 soilHumidity: soilHumidity ?? 0
             )
 //            bluetoothManager.centralManagerDidUpdateState(bluetoothManager.centralManager)
+//            clearFile()
         }
         
     }
@@ -144,7 +156,7 @@ struct ContentView: View {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("Bearer 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", forHTTPHeaderField: "Authorization")
+        request.setValue("Bearer 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", forHTTPHeaderField: "Authorization")
         let body: [String: Any] = [
             "model": "deepseek/deepseek-chat-v3.1:free",
             "messages": [
@@ -211,33 +223,139 @@ struct ContentView: View {
         task.resume()
     }
     func writeNewName() -> Void{
-        let path = "/Users/sergejlarkin/Documents/Flower Tamagochi final/Flower Tamagochi/FlowerName.txt"
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent("FlowerName.txt")
         do {
-            try self.flowerName.write(toFile: path, atomically: true, encoding: .utf8)
+            try self.flowerName.write(to: fileURL, atomically: true, encoding: .utf8)
         }catch{
             print("Error write")
         }
     }
-    func readFromFile()-> String{
-        let path = "/Users/sergejlarkin/Documents/Flower Tamagochi final/Flower Tamagochi/FlowerName.txt"
-        var fileContent: String = ""
+    func writeNewCount() -> Void{
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent("DataCount.txt")
         do {
-            fileContent = try String(contentsOfFile: path, encoding: .utf8)
+            try String(self.countEdits).write(to: fileURL, atomically: true, encoding: .utf8)
+        }catch{
+            print("Error write")
+        }
+    }
+    func readFromFile() -> String {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent("FlowerName.txt")
+        
+        // Проверка существования файла
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+            return "File does not exist"
+        }
+
+        do {
+            let fileContent = try String(contentsOf: fileURL, encoding: .utf8)
             return fileContent
         } catch {
-            return "Error read"
+            return "Error reading file: \(error.localizedDescription)"
+        }
+    }
+    func readEdits() -> String {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent("DataCount.txt")
+        
+        // Проверка существования файла
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+            return "File does not exist"
+        }
+
+        do {
+            let fileContent = try String(contentsOf: fileURL, encoding: .utf8)
+            return fileContent
+        } catch {
+            return "Error reading file: \(error.localizedDescription)"
         }
     }
     func writeNewData(temp: Int, airHumidity: Int, soilHumidity: Int){
-        var fileContent: String = ""
-        var addString = "\(temp), \(airHumidity), \(soilHumidity)"
-        let path = "/Users/sergejlarkin/Documents/Flower Tamagochi final/Flower Tamagochi/FlowerData.txt"
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let addString = "\(temp), \(airHumidity), \(soilHumidity)\n"
+        let fileURL = documentsURL.appendingPathComponent("FlowerData.txt")
+        if let fileHandle = try? FileHandle(forWritingTo: fileURL) {
+            fileHandle.seekToEndOfFile()
+            if let data = addString.data(using: .utf8) {
+                fileHandle.write(data)
+            }
+            try? fileHandle.close()
+        } else {
+            do {
+                try addString.write(to: fileURL, atomically: true, encoding: .utf8)
+            } catch {
+                print("Error update data")
+            }
+        }
+        countEdits += 1
+    }
+    func shrinkFile() {
+        var sum_temp: Int = 0
+        var sum_airHumidity: Int = 0
+        var sum_soilHumidity: Int = 0
+        var dataArray: [String] = []
+        
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent("FlowerData.txt")
+        
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+            print("File does not exist")
+            return
+        }
+        
         do {
-            fileContent = try String(contentsOfFile: path, encoding: .utf8)
-            fileContent += "\n" + addString
-            try fileContent.write(toFile: path, atomically: true, encoding: .utf8)
+            let data = try String(contentsOf: fileURL, encoding: .utf8)
+            dataArray = data.split(separator: "\n").map(String.init)
+            
+            if dataArray.count < 10 {
+                print("Not enough data to shrink.")
+                return
+            }
+
+            for i in dataArray[dataArray.count - 10..<dataArray.count] {
+                let components = i.split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces) }
+                if components.count == 3,
+                   let tempValue = Int(components[0]),
+                   let airHumidityValue = Int(components[1]),
+                   let soilHumidityValue = Int(components[2]) {
+                    sum_temp += tempValue
+                    sum_airHumidity += airHumidityValue
+                    sum_soilHumidity += soilHumidityValue
+                }
+            }
+
+            let average_temp: Int = sum_temp / 10
+            let average_airHumidity: Int = sum_airHumidity / 10
+            let average_soilHumidity: Int = sum_soilHumidity / 10
+
+            print("Average Temp: \(average_temp), Average Air Humidity: \(average_airHumidity), Average Soil Humidity: \(average_soilHumidity)")
+            let addString = "\(average_temp), \(average_airHumidity), \(average_soilHumidity)\n"
+            
+            try "".write(to: fileURL, atomically: true, encoding: .utf8)
+            
+            if let fileHandle = try? FileHandle(forUpdating: fileURL) {
+                fileHandle.seekToEndOfFile()
+                if let data = addString.data(using: .utf8) {
+                    fileHandle.write(data)
+                    print("Data appended: \(addString)")
+                }
+                try? fileHandle.close()
+            }
         } catch {
-            print("Error update data")
+            print("Error reading/writing file: \(error.localizedDescription)")
+        }
+    }
+    func clearFile(){
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent("FlowerData.txt")
+        do {
+            try "".write(to: fileURL, atomically: true, encoding: .utf8)
+            countEdits = 0
+            writeNewCount()
+        }catch{
+            print("Error write")
         }
     }
 }
