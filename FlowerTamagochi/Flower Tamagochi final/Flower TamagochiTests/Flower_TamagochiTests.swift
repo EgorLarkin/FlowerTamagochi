166659e1ece30a8b8197032424af9fdb40e90446
EgorLarkin <166232709+EgorLarkin@users.noreply.github.com>
2025-12-13 00:33:32 +0300

    Add files via upload

---
+++ FlowerTamagochi/Flower Tamagochi final/Flower TamagochiTests/Flower_TamagochiTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flower_TamagochiTests.swift
+//  Flower TamagochiTests
+//
+//  Created by Сергей Ларкин on 09/09/2025.
+//
+
+import Testing
+@testable import Flower_Tamagochi
+
+struct Flower_TamagochiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
