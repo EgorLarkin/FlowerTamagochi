db12be6d22f30cd45d67d7472bbc499a13735e33
EgorLarkin <166232709+EgorLarkin@users.noreply.github.com>
2025-12-13 14:33:12 +0300

    Add files via upload

---
+++ FlowerTamagochi/Flower Tamagochi final/Flower Tamagochi/Flower_TamagochiApp.swift
@@ -0,0 +1,18 @@
+//
+//  Flower_TamagochiApp.swift
+//  Flower Tamagochi
+//
+//  Created by Сергей Ларкин on 09/09/2025.
+//
+
+import SwiftUI
+
+// MARK: - App Entry Point
+@main
+struct Flower_TamagochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
